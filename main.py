b23bdcbb0b5a5fc61b0409764770d43949ab4375
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2021-04-28 03:32:58 +0300

    Allow ignoring seed

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ from models import ActorTD3, ActorDDPG, CriticDDPG, Critic, DualCritic
 # A fixed seed is used for the eval environment
 def eval_policy(policy, env_name, seed, eval_episodes=10):
     eval_env = gym.make(env_name)
-    eval_env.seed(seed + 100)
+    if seed >= 0:
+        eval_env.seed(seed + 100)
 
     avg_reward = 0.
     for _ in range(eval_episodes):
@@ -38,7 +39,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--policy", default="TD3")                  # Policy name (TD3, DDPG or OurDDPG)
     parser.add_argument("--env", default="HalfCheetah-v2")          # OpenAI gym environment name
-    parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
+    parser.add_argument("--seed", default=-1, type=int)              # Sets Gym, PyTorch and Numpy seeds
     parser.add_argument("--start_timesteps", default=25e3, type=int)  # Time steps initial random policy is used
     parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
     parser.add_argument("--max_timesteps", default=1e6, type=int)   # Max time steps to run environment
@@ -67,10 +68,11 @@ if __name__ == "__main__":
     env = gym.make(args.env)
 
     # Set seeds
-    env.seed(args.seed)
-    env.action_space.seed(args.seed)
-    torch.manual_seed(args.seed)
-    np.random.seed(args.seed)
+    if args.seed >= 0:
+        env.seed(args.seed)
+        env.action_space.seed(args.seed)
+        torch.manual_seed(args.seed)
+        np.random.seed(args.seed)
 
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
